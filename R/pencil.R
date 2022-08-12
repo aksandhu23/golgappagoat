d47c7ad7199bddd2c0887094ebd262b7bef8d317
aksandhu23 <amritsandhu707@gmail.com>
2022-08-12 11:37:08 -0700

    Initial commit

---
+++ R/pencil.R
@@ -0,0 +1,13 @@
+#' Title
+#'
+#' @param lead
+#' @param tree
+#' @param paint
+#'
+#' @return
+#' @export
+#'
+#' @examples
+paint <- function(lead, tree, paint) {
+  (lead + tree) / paint
+}
---
+++ R/snacks.R
@@ -0,0 +1,17 @@
+#' Calculates my snacks
+#'
+#' @param grapes mass of grapes (grams)
+#' @param apples number of apples in a bucket
+#' @param burritos how many burritos I eat
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' snacks(grapes = 2.5, apples = 6, burritos = 1)
+
+snacks <- function(grapes, apples, burritos) {
+  (grapes + apples) / burritos
+}
+
+# Run devtools::document() and then go over to Build and click Install and Restart, next check the funtion so in this case I entered: golgappagoat::snacks(grapes = 10, apples = 1, burritos = 3)
